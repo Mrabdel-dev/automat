cc7edd485428cb92ed9129a994874900e2b2dee9
Mrabdel-dev <neravana@gmail.com>
2021-06-01 18:33:57 +0200

    update working in half step

--- start.py
+++ start.py
@@ -1,29 +1,29 @@
-import xlsxwriter
+# import xlsxwriter
 from openpyxl import load_workbook
-#looad your pds file here
-pds =load_workbook('PDS.xlsx')
-wpds= pds.sheetnames
+# load your pds file here
+pds = load_workbook('PDS.xlsx')
+wpds = pds.sheetnames
 
-file="epes.xlsx"
-eps =load_workbook(file)
+file = "epes.xlsx"
+eps = load_workbook(file)
 epsheet = eps.active
 k = epsheet.max_row
 print(k)
-################## the part of coping values from pds to new file ######################
+# ################# the part of coping values from pds to new file ######################
 for s in wpds:
     sheet = pds[s]
-    MaxRow= sheet.max_row
-    MaxCol= sheet.max_column
+    MaxRow = sheet.max_row
+    MaxCol = sheet.max_column
     k = epsheet.max_row
     print('#'*15)
     print(k)
-    for i in range(12,MaxRow+1):
+    for i in range(12, MaxRow+1):
         k = k + 1
-        for j in range(1,MaxCol+1):
+        for j in range(1, MaxCol+1):
 
-            #read valueus
-            valin = sheet.cell(row=i,column=j).value
-            #append the value
-            epsheet.cell(row=k, column=j).value=valin
-file2='uapdte.xlsx'
+            # read values
+            valin = sheet.cell(row=i, column=j).value
+            # append the value
+            epsheet.cell(row=k, column=j).value = valin
+file2 = 'uapdte.xlsx'
 eps.save(file2)

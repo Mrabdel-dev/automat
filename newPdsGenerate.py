582dbbe79a07864b6337f9f57e93d962a2c00ed6
Mrabdel-dev <neravana@gmail.com>
2021-06-03 16:49:02 +0200

    03/06/2021

--- newPdsGenerate.py
+++ newPdsGenerate.py
@@ -1,6 +1,7 @@
 from dbfread import DBF
 import xlsxwriter
 import datetime
+
 # date configuration
 now = datetime.datetime.now()
 date = now.strftime("%d/%m/%Y")
@@ -15,60 +16,97 @@ filedBoiteNam = boiteTable.field_names
 boiteLen = len(boiteTable)
 cableLen = len(cableTable)
 # declare the table that i need te full
-boiteCode = [] ; boiteCable = [] ; boiteCableState = [] ; boiteReference = [] ; nbf = []
-cableName = [] ; cableOrigin = [] ; cableExtremity = [] ; cableCapacity = []
+boiteCode = [];
+boiteCable = [];
+boiteCableState = [];
+boiteReference = [];
+nbf = []
+cableName = [];
+cableOrigin = [];
+cableExtremity = [];
+cableCapacity = []
 
 for i in range(0, cableLen):
-       cableName.append(cableTable.records[i]['NOM'])
-       cableOrigin.append(cableTable.records[i]['ORIGINE'])
-       cableExtremity.append(cableTable.records[i]['EXTREMITE'])
-       cableCapacity.append(cableTable.records[i]['CAPACITE'])
+    cableName.append(cableTable.records[i]['NOM'])
+    cableOrigin.append(cableTable.records[i]['ORIGINE'])
+    cableExtremity.append(cableTable.records[i]['EXTREMITE'])
+    cableCapacity.append(cableTable.records[i]['CAPACITE'])
 for j in range(0, boiteLen):
-       boiteCode.append(boiteTable.records[j]['NOM'])
-       boiteCable.append(boiteTable.records[j]['AMONT'])
-       boiteCableState.append(boiteTable.records[j]['INTERCO'])
-       boiteReference.append(boiteTable.records[j]['REFERENCE'])
+    boiteCode.append(boiteTable.records[j]['NOM'])
+    boiteCable.append(boiteTable.records[j]['AMONT'])
+    boiteCableState.append(boiteTable.records[j]['INTERCO'])
+    boiteReference.append(boiteTable.records[j]['REFERENCE'])
 
 # define the character and style of cell inside excel
-bold = workbook.add_format({'bold': True,"border":1})
-border = workbook.add_format({"border":1})
-back = workbook.add_format({"bg_color": '#CD5C5C' , "border":1})
-cassette = workbook.add_format({"bg_color": '#A9A9A9' ,"border":1})
-cell_formatCapacity = workbook.add_format({"bg_color": '#E6E6FA',"border":1})
-cell_format1 = workbook.add_format({"bg_color": 'red',"border":1})
-cell_format2 = workbook.add_format({"bg_color": 'blue',"border":1})
-cell_format3 = workbook.add_format({"bg_color": '#00FF00',"border":1})
-cell_format4 = workbook.add_format({"bg_color": 'yellow',"border":1})
-cell_format5 = workbook.add_format({"bg_color": '#BF00FF',"border":1})
-cell_format6 = workbook.add_format({"bg_color": 'white',"border":1})
-cell_format7 = workbook.add_format({"bg_color": '#FFBF00',"border":1})
-cell_format8 = workbook.add_format({"bg_color": '#828282',"border":1})
-cell_format9 = workbook.add_format({"bg_color": '#816B56',"border":1})
-cell_format10 = workbook.add_format({"bg_color": '#333333',"border":1})
-cell_format11 = workbook.add_format({"bg_color": '#00FFBF',"border":1})
-cell_format12 = workbook.add_format({"bg_color": '#FFAAD4',"border":1})
+bold = workbook.add_format({'bold': True, "border": 1})
+bold1 = workbook.add_format({'bold': True})
+border = workbook.add_format({"border": 1})
+back = workbook.add_format({"bg_color": '#CD5C5C', "border": 1})
+cassette = workbook.add_format({"bg_color": '#A9A9A9', "border": 1})
+cell_formatCapacity = workbook.add_format({"bg_color": '#E6E6FA', "border": 1})
+cell_format1 = workbook.add_format({"bg_color": 'red', "border": 1})
+cell_format2 = workbook.add_format({"bg_color": 'blue', "border": 1})
+cell_format3 = workbook.add_format({"bg_color": '#00FF00', "border": 1})
+cell_format4 = workbook.add_format({"bg_color": 'yellow', "border": 1})
+cell_format5 = workbook.add_format({"bg_color": '#BF00FF', "border": 1})
+cell_format6 = workbook.add_format({"bg_color": 'white', "border": 1})
+cell_format7 = workbook.add_format({"bg_color": '#FFBF00', "border": 1})
+cell_format8 = workbook.add_format({"bg_color": '#828282', "border": 1})
+cell_format9 = workbook.add_format({"bg_color": '#816B56', "border": 1})
+cell_format10 = workbook.add_format({"bg_color": '#333333', "border": 1})
+cell_format11 = workbook.add_format({"bg_color": '#00FFBF', "border": 1})
+cell_format12 = workbook.add_format({"bg_color": '#FFAAD4', "border": 1})
+colorList = [cell_format1, cell_format2, cell_format3, cell_format4, cell_format5, cell_format6, cell_format7,
+             cell_format8, cell_format9, cell_format10, cell_format11, cell_format12]
 
-for b in range(0,boiteLen):
-       w = workbook.add_worksheet(boiteCode[b])
-       w.write('A7', 'Etiquette : ' + boiteCode[b], border)
-       w.write('O3', date, bold)
-       w.write('N3', 'Date de modification : ', bold)
-       w.write('A11', 'Entrée', bold)
-       w.write('B11', 'Capacité', bold)
-       w.write('C11', 'N°         ', bold)
-       w.write('D11', 'N° Tube', bold)
-       w.write('E11', 'N° Fibre', bold)
-       w.write('F11', 'Cassette', bold)
-       w.write('G11', 'Etat fibre', bold)
-       w.write('H11', 'N° Fibre', bold)
-       w.write('I11', 'N° Tube', bold)
-       w.write('J11', 'N°       ', bold)
-       w.write('K11', 'Capacité', bold)
-       w.write('L11', '', bold)
-       w.write('M11', 'Sortie', bold)
-       w.write('N11', 'Statut', bold)
-       w.write('O11', 'Client', bold)
-       w.write('A9', 'Reference : ' + boiteReference[b], border)
-       w.write('A1', '<- RETOUR:', back)
+
+for b in range(0, boiteLen):
+    N = 1
+    n = 1
+    w = workbook.add_worksheet(boiteCode[b])
+    # INFORMATION ABOUT BOITE
+    w.write('Q1', 'Etiquette : ', border)
+    w.write('R1', boiteCode[b], bold)
+    w.write('Q2', 'Reference : ', border)
+    w.write('R2', boiteReference[b], bold)
+    w.write('Q3', 'Date de modification : ', bold)
+    w.write('R3', date, bold)
+    # INFORMATION OF THE HEADER
+    w.write('A1', 'Entrée', bold)
+    w.write('B1', 'Capacité', bold)
+    w.write('C1', 'N°         ', bold)
+    w.write('D1', 'N° Tube', bold)
+    w.write('E1', 'N° Fibre', bold)
+    w.write('F1', 'Cassette', bold)
+    w.write('G1', 'Etat fibre', bold)
+    w.write('H1', 'N° Fibre', bold)
+    w.write('I1', 'N° Tube', bold)
+    w.write('J1', 'N°       ', bold)
+    w.write('K1', 'Capacité', bold)
+    w.write('L1', '', bold)
+    w.write('M1', 'Sortie', bold)
+    w.write('N1', 'Statut', bold)
+    w.write('O1', 'Client', bold)
+    indexCable = cableName.index(boiteCable[b])
+    cap = cableCapacity[indexCable]
+    for c in range(2, cap + 2):
+        w.write('A' + str(c), cableName[indexCable], bold1)
+        w.write('B' + str(c), cap, cell_formatCapacity)
+        if n == 12:
+            w.write('C' + str(c), n)
+            w.write('E' + str(c), n, colorList[n - 1])
+            w.write('D' + str(c), N, colorList[N - 1])
+            n = 1
+            if N==12:
+                N=1
+            else :
+                N = N+1
+
+        else:
+            w.write('C' + str(c), n)
+            w.write('E' + str(c), n, colorList[n - 1])
+            w.write('D' + str(c), N, colorList[N - 1])
+            n = n + 1
+
 
 workbook.close()

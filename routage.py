05c250aa75aabf082b59e215a61fec44cfc37ef7
Mrabdel-dev <neravana@gmail.com>
2021-06-06 03:39:49 +0100

    LAST WORK

--- routage.py
+++ routage.py
@@ -1,14 +1,16 @@
+import string
+
 import xlsxwriter
 from openpyxl import load_workbook
 
-rootBook = xlsxwriter.Workbook('fileGenerated/roote.xlsx')
+# ################define the path of output routage file######################################
+rootBook = xlsxwriter.Workbook('fileGenerated/root.xlsx')
 wr = rootBook.add_worksheet()
 # define the character and style of cell inside excel
 bold = rootBook.add_format({'bold': True, "border": 1})
 bold1 = rootBook.add_format({'bold': True})
 border = rootBook.add_format({"border": 1})
 header = rootBook.add_format({'bold': True, 'border': 1, 'bg_color': '#037d50'})
-back = rootBook.add_format({"bg_color": '#CD5C5C', "border": 1})
 cassette = rootBook.add_format({"bg_color": '#A9A9A9', "border": 1})
 cell_formatCapacity = rootBook.add_format({"bg_color": '#E6E6FA', "border": 1})
 cell_format1 = rootBook.add_format({"bg_color": 'red', "border": 1})
@@ -24,7 +26,23 @@ cell_format10 = rootBook.add_format({"bg_color": '#333333', "border": 1})
 cell_format11 = rootBook.add_format({"bg_color": '#00FFBF', "border": 1})
 cell_format12 = rootBook.add_format({"bg_color": '#FFAAD4', "border": 1})
 colorList = [cell_format1, cell_format2, cell_format3, cell_format4, cell_format5, cell_format6, cell_format7,
-             cell_format8, cell_format9, cell_format10, cell_format11, cell_format12]
+             cell_format8, cell_format9, cell_format10, cell_format11, cell_format12, border]
+
+
+def stringCassette(x: string):
+    if x.isdigit():
+        j=0
+        for i in range(0, 13):
+            if i == int(x):
+                x = i
+                j=1
+        if j ==1:
+            print(x)
+            return colorList[x-1]
+        else:
+            print(x)
+            return colorList[12]
+    return colorList[len(colorList) - 1]
 
 
 def baseHeader():
@@ -35,24 +53,30 @@ def baseHeader():
     wr.write('E1', 'TIROIR', header)
     wr.write('F1', 'TYPE', header)
 
+
 def normalHeader(i):
-    wr.write('G'+str(i), 'CAS', header)
-    wr.write('H'+str(i), 'T', header)
-    wr.write('I'+str(i), 'F', header)
-    wr.write('J'+str(i), 'CABLE', header)
-    wr.write('K'+str(i), 'BOITE', header)
-    wr.write('L'+str(i), 'TYPE', header)
+    wr.write('G' + str(i), 'CAS', header)
+    wr.write('H' + str(i), 'T', header)
+    wr.write('I' + str(i), 'F', header)
+    wr.write('J' + str(i), 'CABLE', header)
+    wr.write('K' + str(i), 'BOITE', header)
+    wr.write('L' + str(i), 'TYPE', header)
+
+
+# ##################declare the input PDS file ############################
 pdsBook = load_workbook('fileGenerated/PDS.xlsx')
 pdsSheets = pdsBook.sheetnames
+# boite base of sro
 sheetSro = []
+# cable base sro
 cableSro = []
-capSro= []
-value = ''
+# capacity of the cable sro
+capSro = []
+# sro name
+SRO = ''
+# loop to the boite inside the pds to know SRO cable
 for sh in pdsSheets:
     sheet = pdsBook[sh]
-    print(sh)
-    print(sheet.max_row-11)
-    print('#'*15)
     value = sheet.cell(row=1, column=1).value
     if str(value).startswith('SRO'):
         sheetSro.append(sh)
@@ -61,40 +85,55 @@ for sh in pdsSheets:
         cableSro.append(str(sheet.cell(row=12, column=1).value))
         capSro.append(int(sheet.cell(row=12, column=3).value))
 
-    else:
-        pass
 baseHeader()
 normalHeader(1)
-p=0
-c=[]
-for i in range(65,77):
+p = 0
+c = []
+for i in range(65, 77):
     c.append(chr(i))
 c.append(chr(78))
-L=0
-T=0
-f=0
-N=2
-Len=0
+L = 0
+T = 0
+f = 0
+N = 0
+Len = 0
 for b in sheetSro:
     bshet = pdsBook[b]
-    L=1
-    T=T+1
+    L = 1
+    T = T + 1
     Len = Len + bshet.max_row - 11
-    for p in range(N,Len+2):
-        wr.write('A'+str(p),value, border)
-        wr.write('B' + str(p),p, border)
-        wr.write('E' + str(p), 'TIROIR_'+str(T), border)
+    for p, s in zip(range(N + 2, Len + 2), range(12, bshet.max_row + 1)):
+        if f == 13:
+            f = 0
+        wr.write('A' + str(p), SRO, border)
+        wr.write('B' + str(p), p-1, border)
+        wr.write('E' + str(p), 'TIROIR_' + str(T), border)
         wr.write('F' + str(p), 'CONNECTEUR', border)
         wr.write('C' + str(p), c[f], border)
         wr.write('D' + str(p), L, border)
-
-        if p%6 ==0:
-            if L==6:
-                L=1
-            elif L <6:
-                L = L+1
-        else :
-            pass
-
-
-
+        x = bshet.cell(row=s, column=5).value
+        wr.write('G' + str(p), x, cassette)
+        x = bshet.cell(row=s, column=5).value
+        wr.write('H' + str(p), x, stringCassette(str(x)))
+        x = bshet.cell(row=s, column=6).value
+        wr.write('I' + str(p), x, stringCassette(str(x)))
+        x = bshet.cell(row=12, column=1).value
+        wr.write('J' + str(p), x, border)
+        x = bshet.cell(row=7, column=1).value
+        wr.write('K' + str(p), x, border)
+        x = bshet.cell(row=s, column=8).value
+        wr.write('L' + str(p), x, border)
+        x = bshet.cell(row=s, column=7).value
+        wr.write('M' + str(p), x, cassette)
+        x = bshet.cell(row=s, column=10).value
+        wr.write('N' + str(p), x, stringCassette(str(x)))
+        x = bshet.cell(row=s, column=9).value
+        wr.write('O' + str(p), x, stringCassette(str(x)))
+        f = f + 1
+        if p % 24 == 0:
+            if L == 6:
+                L = 1
+            elif L < 6:
+                L = L + 1
+    N = Len
+rootBook.close()
--- try.py
+++ try.py
@@ -6,4 +6,9 @@
 # listi = ['1','2','158','99','93']
 # for i in enumerate(listi):
 #     print(i)
-print(chr(77))
+c = []
+for i in range(65, 77):
+    c.append(chr(i))
+c.append(chr(78))
+
+print(c[15])

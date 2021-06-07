75ec9da96cdbdb76a152fddf46194d213252c230
Mrabdel-dev <neravana@gmail.com>
2021-06-07 18:36:24 +0200

    last update today : monday need to remove the libre type

--- try.py
+++ try.py
@@ -12,8 +12,17 @@
 # c.append(chr(78))
 #
 # print(c[15])
-import xlsxwriter
-rootBook = xlsxwriter.Workbook('fileGenerated/hh.xlsx')
-wr = rootBook.add_worksheet()
-wr.write(0,0,"hello")
-rootBook.close()
+# import xlsxwriter
+# rootBook = xlsxwriter.Workbook('fileGenerated/hh.xlsx')
+# wr = rootBook.add_worksheet()
+# wr.write(0,0,"hello")
+# rootBook.close()
+# import operator
+#
+# az ={'rrrrr':144,'hhhhhh':568,'yyyyyyyyy':144}
+# sortedOne =dict(sorted(az.items(), key=operator.itemgetter(1)))
+#
+# sh=sortedOne.keys()
+# print(sortedOne)
+# for sh in sh :
+#     print(sh)

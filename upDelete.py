bc7469b357b7742a4ac5d296f23baac1fc8a4d35
Mrabdel-dev <neravana@gmail.com>
2021-08-12 18:33:00 +0100

    last uapdate at all

--- upDelete.py
+++ upDelete.py
@@ -1,13 +1,13 @@
 from openpyxl import load_workbook
 
 # load the old book you gona modify
-workbookOldFree = load_workbook('C:/Users/etudes20/Desktop/free/Suivi Projet FREE-IDF 21-06.xlsx')
+workbookOldFree = load_workbook('C:/Users/etudes20/Desktop/free/Suivi Projet FREE-IDF-09-07.xlsx')
 oldBookFree = workbookOldFree.active
 # load the old book you gona modify
-workbookOldPaca = load_workbook('C:/Users/etudes20/Desktop/free/Suivi projets Free  PACA 28-06.xlsx')
+workbookOldPaca = load_workbook('C:/Users/etudes20/Desktop/free/Suivi projets Free PACA-05-08.xlsx')
 oldBookPaca = workbookOldPaca.active
 # load the new book that you want get value from it
-workbookNew = load_workbook('C:/Users/etudes20/Desktop/free/export_projets_2021-06-29-12-13-13.xlsx')
+workbookNew = load_workbook('C:/Users/etudes20/Desktop/free/export_projets_2021-08-12-16-05-50.xlsx')
 newBook = workbookNew.active
 # define parameter for loop
 maxRow = newBook.max_row
@@ -65,43 +65,43 @@ for i in range(2, maxRow + 1):
 
 newMax = oldBookFree.max_row
 # the second loop is for delete all value doesn't come with new file
-for c in range(2, newMax):
-    Test = str(oldBookFree.cell(row=c, column=6).value)
-    if Test == "Sebastien GELSI":
-        valTest = oldBookFree.cell(row=c, column=4).value
-        for d in range(2, maxRow + 1):
-            valTestedWith = newBook.cell(row=d, column=4).value
-            if valTest == valTestedWith:
-                y = 1
-                break
-            else:
-                y = 0
-        if y == 0:
-            NbrDelFree = NbrDelFree + 1
-            for e in range(1, maxColOldFree + 1):
-                oldBookFree.cell(row=c, column=e).value = ''
-
-newMaxPaca = oldBookPaca.max_row
-for c in range(2,newMaxPaca):
-    Test = str(oldBookPaca.cell(row=c, column=6).value)
-    if Test == "Philippe PHILIS":
-        print(c)
-        valTest = oldBookPaca.cell(row=c, column=4).value
-        for d in range(2, maxRow + 1):
-            valTestedWith = newBook.cell(row=d, column=4).value
-            if valTest == valTestedWith:
-                y = 1
-                break
-            else:
-                y = 0
-        if y == 0:
-            NbrDelPaca = NbrDelPaca + 1
-            for e in range(1, maxColOldPaca + 1):
-                oldBookPaca.cell(row=c, column=e).value = ''
-    else:
-        print("yes")
-        for e in range(1, maxColOldPaca + 1):
-            oldBookPaca.cell(row=c, column=e).value = ''
+# for c in range(2, newMax):
+#     Test = str(oldBookFree.cell(row=c, column=6).value)
+#     if Test == "Sebastien GELSI":
+#         valTest = oldBookFree.cell(row=c, column=4).value
+#         for d in range(2, maxRow + 1):
+#             valTestedWith = newBook.cell(row=d, column=4).value
+#             if valTest == valTestedWith:
+#                 y = 1
+#                 break
+#             else:
+#                 y = 0
+#         if y == 0:
+#             NbrDelFree = NbrDelFree + 1
+#             for e in range(1, maxColOldFree + 1):
+#                 oldBookFree.cell(row=c, column=e).value = ''
+#
+# newMaxPaca = oldBookPaca.max_row
+# for c in range(2,newMaxPaca):
+#     Test = str(oldBookPaca.cell(row=c, column=6).value)
+#     if Test == "Philippe PHILIS":
+#         print(c)
+#         valTest = oldBookPaca.cell(row=c, column=4).value
+#         for d in range(2, maxRow + 1):
+#             valTestedWith = newBook.cell(row=d, column=4).value
+#             if valTest == valTestedWith:
+#                 y = 1
+#                 break
+#             else:
+#                 y = 0
+#         if y == 0:
+#             NbrDelPaca = NbrDelPaca + 1
+#             for e in range(1, maxColOldPaca + 1):
+#                 oldBookPaca.cell(row=c, column=e).value = ''
+#     else:
+#         print("yes")
+#         for e in range(1, maxColOldPaca + 1):
+#             oldBookPaca.cell(row=c, column=e).value = ''
 
 
 print('######## the number of values in the old file free ########')
@@ -122,7 +122,7 @@ print(f'number of element exist already is : ==>{NbrExsPaca}')
 print('#################### the number of values in the old file after update ####################')
 print(oldBookPaca.max_row - 1)
 # save the file
-fileNFree = 'fileGenerated/Suivi Projet FREE-IDF-Nouveau.xlsx'
-fileNPaca = 'fileGenerated/Suivi projets Free PACA-Nouveau.xlsx'
+fileNFree = 'C:/Users/etudes20/Desktop/free/Suivi Projet FREE-IDF-Nouveau.xlsx'
+fileNPaca = 'C:/Users/etudes20/Desktop/free/Suivi projets Free PACA-Nouveau.xlsx'
 workbookOldFree.save(fileNFree)
 workbookOldPaca.save(fileNPaca)
